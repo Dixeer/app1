4156f1bec3111d999d4d00ea2a4ac39bf214d066
Damian <damian.zmuda22@gmail.com>
2017-03-15 14:01:19 +0100

    end

--- app/src/main/java/com/example/damian/app1/MainActivity.java
+++ app/src/main/java/com/example/damian/app1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.damian.app1;
 
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -26,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (!nameEditText.isEnabled()) {
+            changeColor(nameTextView);
+        }
+    }
 
+    private void changeColor(TextView name) {
+        int currentColour = name.getCurrentTextColor();
+        if(currentColour == ResourcesCompat.getColor(getResources(), R.color.green, null)){
+            name.setTextColor(ResourcesCompat.getColor(getResources(), R.color.red, null));
+        }else if(currentColour == ResourcesCompat.getColor(getResources(), R.color.red, null)){
+            name.setTextColor(ResourcesCompat.getColor(getResources(), R.color.blue, null));
+        }else{
+            name.setTextColor(ResourcesCompat.getColor(getResources(), R.color.green, null));
         }
     }
 }
